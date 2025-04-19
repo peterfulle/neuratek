4f858dbf564f23b1b37de4c34e3e452ff62f414e
peterfulle <128112243+peterfulle@users.noreply.github.com>
2025-04-19 01:51:16 -0400

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,15 +1,12 @@
 // config.js
 const isDevelopment = window.location.hostname === "localhost" || 
                       window.location.hostname === "127.0.0.1";
-
 let baseUrl;
-
 if (isDevelopment) {
   // URL para desarrollo local
   baseUrl = "http://localhost:8000";
 } else {
-  // URL de tu proxy personalizado en Cloudflare Workers
-  baseUrl = "https://neuratek.alejandroiglesias.workers.dev";
+  // URL directa al backend de Azure
+  baseUrl = "https://neuratek-backend-e7haeehjfmfkhchv.canadacentral-01.azurewebsites.net";
 }
-
 export const API_BASE_URL = baseUrl;
